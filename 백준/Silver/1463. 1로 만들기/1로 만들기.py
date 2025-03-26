7ac216109fc5d67d93972be73484daf51c6d6b03
권길현 <gilhyun.gwon0731@gmail.com>
2025-03-26 22:20:55 +0900

    [Silver III] Title: 1로 만들기, Time: 1288 ms, Memory: 127392 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,20 @@
+import sys
+sys.setrecursionlimit(10**9)
+def f(n):
+    if room[n] != 10**5:
+        return room[n]
+    a,b,c = 10**5,10**5,10**5
+    if(n % 2 == 0):
+        a = f(n//2)+1
+    if(n % 3 == 0):
+        b = f(n//3) + 1
+    c = f(n-1) + 1
+    room[n] = min(a,b,c)
+    return room[n]
+
+n = int(input())
+room = [10**5] * (10**6 + 1)
+room[1] = 0
+room[2] = 1
+room[3] = 1
+print(f(n))
