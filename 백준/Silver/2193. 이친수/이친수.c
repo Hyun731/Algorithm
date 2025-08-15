e743de3ef56dcde7fe7a6bf4df2153c03a62ed35
권길현 <gilhyun.gwon0731@gmail.com>
2025-08-15 15:17:15 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    long long dp[91] = {0};
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    printf("%lld\n", dp[n]);
+    return 0;
+}
